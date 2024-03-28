449123daa3898b2ef115234e0e2b24e08e3e2472
Martin Milde <sidewindernor@gmail.com>
2024-03-28 15:32:08 +0100

    Refactored some functions in MovingPlatform.

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -1,14 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MovingPlatform.h"
 
 // Sets default values
 AMovingPlatform::AMovingPlatform()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
@@ -17,7 +15,6 @@ void AMovingPlatform::BeginPlay()
 	Super::BeginPlay();
 
 	StartLocation = GetActorLocation();
-
 }
 
 // Called every frame
@@ -32,23 +29,36 @@ void AMovingPlatform::Tick(float DeltaTime)
 
 void AMovingPlatform::MovePlatform(float DeltaTime)
 {
-	FVector CurrentLocation = GetActorLocation();
-	CurrentLocation += PlatformVelocity * DeltaTime;
 
-	SetActorLocation(CurrentLocation);
-
-	float PlatformDistanceTravelled = FVector::Dist(StartLocation, CurrentLocation);
-
-	if(PlatformDistanceTravelled >= PlatformMaxDistance)
+	if (ShouldPlatformReturn())
 	{
 		FVector NormalizedDirection = PlatformVelocity.GetSafeNormal();
 		StartLocation = StartLocation + NormalizedDirection * PlatformMaxDistance;
 		SetActorLocation(StartLocation);
 		PlatformVelocity = -PlatformVelocity;
 	}
+
+	else
+	{
+		FVector CurrentLocation = GetActorLocation();
+		CurrentLocation += PlatformVelocity * DeltaTime;
+		SetActorLocation(CurrentLocation);
+	}
+}
+
+bool AMovingPlatform::ShouldPlatformReturn()
+{
+	return GetDistanceMoved() >= PlatformMaxDistance;
+}
+
+float AMovingPlatform::GetDistanceMoved()
+{
+	return FVector::Dist(StartLocation, GetActorLocation());
 }
 
 void AMovingPlatform::RotatePlatform(float DeltaTime)
 {
 	UE_LOG(LogTemp, Warning, TEXT("You Spin Me Right Round"));
 }
+
+
